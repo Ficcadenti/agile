3b44dd0a919f24988faf4b137337a05f58f3b9a5
Raffaele Ficcadenti <raffaele.ficcadenti@asdc.asi.it>
2016-09-09 12:38:28 +0200

    Gestione collaudo - clone dati originali

--- storage1/ricrea_link.sh
+++ storage1/ricrea_link.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-echo "Cancello eventuali link"
+echo "Elimino  /storage1/agile"
+rm -rf /storage1/agile
 
-rm agile/agile3/qlstd/1609050005_1609050119-253638/FM_FLG/PKP048495_1_3901_000_1473040447.flg.gz
-rm agile/agile3/qlstd/1609050005_1609050119-253638/FT3AB_FLG/PKP048495_1_3901_000_1473040447.flg.gz
+echo "Creo directory e file agile2"
+install -D  agile/agile2/qlstd/1609050005_1609050119-145098/FLG__FM/PKP048495_1_3901_000_1473040447.flg.gz /storage1/agile/agile2/qlstd/1609050005_1609050119-145098/FLG__FM/PKP048495_1_3901_000_1473040447.flg.gz
+install -D  agile/agile2/qlstd/1609050005_1609050119-145098/FLG__FT3AB/PKP048495_1_3901_000_1473040447.flg.gz /storage1/agile/agile2/qlstd/1609050005_1609050119-145098/FLG__FT3AB/PKP048495_1_3901_000_1473040447.flg.gz
+
+echo "Creo directory agile3"
+mkdir -p /storage1/agile/agile3/qlstd/1609050005_1609050119-253638/FM_FLG/
+mkdir -p /storage1/agile/agile3/qlstd/1609050005_1609050119-253638/FT3AB_FLG/
+
+echo "Cancello eventuali link"
+rm /storage1/agile/agile3/qlstd/1609050005_1609050119-253638/FM_FLG/PKP048495_1_3901_000_1473040447.flg.gz
+rm /storage1/agile/agile3/qlstd/1609050005_1609050119-253638/FT3AB_FLG/PKP048495_1_3901_000_1473040447.flg.gz
 
 echo "Ricreo i link"
-ln -s ../../../../agile2/qlstd/1609050005_1609050119-145098/FLG__FM/PKP048495_1_3901_000_1473040447.flg.gz agile/agile3/qlstd/1609050005_1609050119-253638/FM_FLG/PKP048495_1_3901_000_1473040447.flg.gz
-ln -s ../../../../agile2/qlstd/1609050005_1609050119-145098/FLG__FT3AB/PKP048495_1_3901_000_1473040447.flg.gz agile/agile3/qlstd/1609050005_1609050119-253638/FT3AB_FLG/PKP048495_1_3901_000_1473040447.flg.gz
+ln -s /storage1/agile/agile2/qlstd/1609050005_1609050119-145098/FLG__FM/PKP048495_1_3901_000_1473040447.flg.gz /storage1/agile/agile3/qlstd/1609050005_1609050119-253638/FM_FLG/PKP048495_1_3901_000_1473040447.flg.gz
+ln -s /storage1/agile/agile2/qlstd/1609050005_1609050119-145098/FLG__FT3AB/PKP048495_1_3901_000_1473040447.flg.gz /storage1/agile/agile3/qlstd/1609050005_1609050119-253638/FT3AB_FLG/PKP048495_1_3901_000_1473040447.flg.gz
+
+echo "Copio DRIFT file"
+
+install -D agile/agile2//LV1corr/048495/VC1/DRIFT-PKP048495_1_33XY_000.lv1.cor /storage1/agile/agile2//LV1corr/048495/VC1/DRIFT-PKP048495_1_33XY_000.lv1.cor
 
